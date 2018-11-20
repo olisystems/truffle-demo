8e0237a0969b2a42cb35ac269b791780f86c8653
Muhammad Yahya <yahya_gis@hotmail.com>
2018-11-20 13:55:04 +0100

    files added

--- truffle.js
+++ truffle.js
@@ -12,14 +12,14 @@
  *   },
  */
 
- module.exports = {
-   // See <https://truffleframework.com/docs/advanced/configuration>
-   // to customize your Truffle configuration!
-   networks: {
-     development: {
-       host: "127.0.0.1",
-       port: 8545,
-       network_id: "*" // Match any network id
-     }
-   }
- };
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks:{
+    development:{
+      host:'127.0.0.1',
+      port:8545,
+      network_id: '*'
+    }
+  }
+};
